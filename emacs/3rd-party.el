4bdc6e50d21d796d060c9affba07bd3e3af2a9aa
Masahito Yoshida <masahito@axsh.net>
2012-03-28 10:38:49 +0900

    add markdown-mode.

--- emacs/3rd-party.el
+++ emacs/3rd-party.el
@@ -1,2 +1,8 @@
 ;; accel !!!
 (load "~/.emacs.d/3rd-party/accel.el")
+
+
+;; markdown
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+      (cons '("\\.text" . markdown-mode) auto-mode-alist))
