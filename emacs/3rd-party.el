6759d93b49d1a9065bd44f9eab918828b325b7b8
Masahito Yoshida <masahito@axsh.net>
2012-08-01 12:19:07 +0900

    emacs: change the pattern match of suffix for markdown-mode condition.

--- emacs/3rd-party.el
+++ emacs/3rd-party.el
@@ -6,4 +6,4 @@
 ;; markdown
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
-      (cons '("\\.text" . markdown-mode) auto-mode-alist))
+      (cons '("\\.md" . markdown-mode) auto-mode-alist))
