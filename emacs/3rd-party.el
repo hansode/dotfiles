c8bacdeae9891b1ab6c2c6f5f917f3d395c4940e
Masahito Yoshida <masahito@axsh.net>
2015-01-03 14:13:57 +0900

    remove markdown-mode.el deployment.

--- emacs/3rd-party.el
+++ emacs/3rd-party.el
@@ -1,9 +1,3 @@
 ;; accel !!!
 ; accel.el does not work on emacs 24.1.
 ;(load "~/.emacs.d/3rd-party/accel.el")
-
-
-;; markdown
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
